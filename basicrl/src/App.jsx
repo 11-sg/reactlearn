6d637844a40ce2e1ac1ea133d8085e6068738b76
11-sg <sg4242674@gmail.com>
2025-11-23 11:50:29 +0530

    day2

---
+++ basicrl/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1> chai aur react vite |
+        
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ basicrl/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+        <h1> fuck you nigash</h1>
+    )
+}
+
+export default Chai
