6eea53db7d0758b74bac83a308560b8720fa637d
11-sg <sg4242674@gmail.com>
2025-11-23 18:42:13 +0530

    2.1

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -4,24 +4,41 @@ import { useState } from 'react'
 
 function App() {
 
-  let [count , setcounter] = useState(0)
+  let [count, setcounter] = useState(0)
+
 
   // let count = 0
 
   const addValue = () => {
-    count = count+1
-    setcounter(count)
+    // count = count+1
+    setcounter(count + 1)
     console.log(count)
   }
 
+  const removeValue = () => {
+    count = count - 1
+    if (count >= 0)
+      setcounter(count)
+    else {
+      setcounter(0)
+      count = 0
+    }
+  }
+
+  const reset= ()=> {
+      setcounter(0)
+  }
   return (
     <>
-      <h1>chai aur react</h1>
       <h2>Counter value: {count} </h2>
       <button
         onClick={addValue}
-        >Add value</button>
-      <button>Remove value</button>
+      >Add value</button>
+      <button
+        onClick={removeValue}
+      >Remove value</button>
+      <br></br>
+      <button onClick={reset}>Reset</button>
     </>
   )
 }
