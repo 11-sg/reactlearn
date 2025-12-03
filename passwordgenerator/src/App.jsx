179ba493d7fd1a36a403b8d5d617ab6b33e006ec
11-sg <sg4242674@gmail.com>
2025-12-04 01:17:31 +0530

    6.1

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length,setlength] = useState(8)
+  const [num , setnum] = useState(false)
+  const [chars,setchars]=useState(false)
+  const [password,setpassword] = useState("")
+  const passwordgen = useCallback(() => {
+    let pass = ""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(num)
+    {
+      str+="0123456789"
+    }
+    if(chars)
+      str+="!@#$%^&*()_+<>:;"
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random()*str.length +1)
+      pass = str.charAt(char)
+    }
+
+    setpassword(pass)
+
+  }, [length,num,chars,setpassword])
+
 
   return (
     <>
-      <h1 className='text-4xl text-center'>Password Generator</h1>
+     <div className='w-full max-w-md mx-auto shadow-md rounded-lg text-orange-500 bg-white'>test</div>
     </>
   )
 }
