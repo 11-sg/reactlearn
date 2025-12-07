a9ec7497004c046e4d1e239c9786e1ae28fd95d8
11-sg <sg4242674@gmail.com>
2025-12-08 01:43:41 +0530

    7.0

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -29,7 +29,13 @@ function App() {
 
   return (
     <>
-     <div className='w-full max-w-md mx-auto shadow-md rounded-lg text-orange-500 bg-white'>test</div>
+     <div className='w-full max-w-md mx-auto shadow-md rounded-lg p-2 px-10 text-orange-500 bg-white'>Password Generator
+
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type ="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readonly></input>
+
+      </div>
+     </div>
     </>
   )
 }
