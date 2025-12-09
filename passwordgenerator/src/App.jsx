97d5bba5ae9aa67649dbb33ebf53d7a83f906ee4
11-sg <sg4242674@gmail.com>
2025-12-10 01:45:13 +0530

    8.1

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 
 import './App.css'
 
@@ -19,22 +19,23 @@ function App() {
 
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char)
+      pass += str.charAt(char)
     }
 
     setpassword(pass)
 
-  }, [length, num, chars, setpassword])
+  }, [length, num, chars])
 
+  useEffect(() => { passwordgen() }, [length, num, chars, passwordgen])
 
   return (
     <>
-      <div className='flex-col justify-center items-center w-full max-w-md mx-auto shadow-md rounded-lg p-5 m-10 text-orange-500  bg-white '>
+      <div className='flex-col justify-center items-center w-full max-w-md mx-auto shadow-md rounded-lg p-5 m-10 text-orange-500  bg-black '>
 
-        <div className='flex justify-center items-center text-2xl p-2 text-black'>
+        <div className='flex justify-center items-center text-2xl p-2 text-white'>
           <p>Password Generator</p></div>
         <br></br>
-        <div className='flex shadow rounded-lg overflow-hidden mb-4 text-black'>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4 text-orange-700'>
           <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readonly></input>
           <button className='bg-blue-700 text-white p-2'>Copy</button>
         </div>
